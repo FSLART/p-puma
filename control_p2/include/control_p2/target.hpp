6cd95df11e1fb2b268c30db3e515aeb215060b0c
TomasMSantos <tomasmarcelinosantos@gmail.com>
2025-09-30 12:14:34 +0100

    Further implementation for the target Class

--- control_p2/include/control_p2/target.hpp
+++ control_p2/include/control_p2/target.hpp
@@ -8,13 +8,17 @@
 class Target{
     public:
         Target(float mission_speed);
+        void set_ready();
+        void set_maxSpeed(float speed);
     private:
         Pursuit_Algorithm algorithm;
     protected:
         // Parameters
-        float mission_speed;
+        float missionSpeed;
         bool ready = false;
-        bool mission_set = false;
+        bool missionSet = false;
+        uint8_t currentSpeed;
+        float currentSteering;
 
     
 };
--- control_p2/src/target.cpp
+++ control_p2/src/target.cpp
@@ -6,12 +6,50 @@ Target::Target(float mission_speed){
 }
 
 lart_msgs::msg::DynamicsCMD Target::getDynamicsCMD(){
-    
-    // CODE FOR CALCULATING THE DYNAMICSCMD
-
     // CALLS MULTIPLE FUNCTIONS ACORDING TO THE NECESSITIES OF THE FLAGS
     
+    if(this->ready && this->mission_set){
+
+        float steeringAngle = calculate_steeringAngle(this->path);
+
+        float speed = calculate_speed(/* PARAMETERS */);
+
+        //create dispatcher with speed and steering
+        lart_msgs::msg::DynamicsCMD controlOutput = lart_msgs::msg::DynamicsCMD();
+        controlOutput.rpm = speed;
+        controlOutput.steering_angle = steeringAngle;
+
+    }
 }
+
+void Target::set_ready(){
+    this->ready = true;
+}
+
+void Target::set_maxSpeed(float speed){
+    this->missionSpeed = speed;
+    this->missionSet = true;
+}
+
+void Target::set_path(lart_msgs::msg::PathSpline path){
+    this->currentPath = path;
+}
+
+void Target::set_dynamics(lart_msgs::msg::Dynamics dynamics){
+    this->currentSpeed = dynamics.rpm;
+    this->currentSteering = dynamics.steering_angle;
+}
+
+void Target::set_pose(geometry_msgs::msg::PoseStamped pose){
+    this->currentPose = pose;
+}
+
+int Target::calculate_speed(lart_msgs::msg::PathSpline path, uint8_t currentSpeed, float missionSpeed){
+
+    return 0;
+}
+
+
 
 // Add a lot of diferent options that can be activated with flags
 // Mostly an option to have a difrent aproach to the speed control

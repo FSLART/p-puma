2dc2a838f3415dcecfdc4e168d5a88056b81a37d
TomasMSantos <tomasmarcelinosantos@gmail.com>
2026-04-10 22:00:34 +0100

    Testing with slam

--- control_p2/include/control_p2/utils.hpp
+++ control_p2/include/control_p2/utils.hpp
@@ -5,7 +5,7 @@
 /*                              ALGORITHM & MODEL                               */
 /*------------------------------------------------------------------------------*/
 
-#define ALGORITHM  "math/lp_pursuit.hpp"
+#define ALGORITHM  "math/acc_pursuit.hpp"
 #define MODEL "model/dry_model.hpp"
 
 
